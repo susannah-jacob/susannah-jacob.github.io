ed89364a2bd346bbdc05b731480662ef8a231531
Madeleine Bruster <maddy.bruster@gmail.com>
2024-11-30 15:10:20 -0500

    cname configuration

--- eleventy.config.js
+++ eleventy.config.js
@@ -21,6 +21,7 @@ export default async function(eleventyConfig) {
 			"./public/": "/"
 		})
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
+		
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
